a58287ef910ecfea5ca763820df176a8747b0105
Mac <mac@MacdeMacBook-Pro.local>
2021-06-12 14:27:28 +0800

    Introduce-Left-Item组件

---
+++ src/components/content/introduceBar/IntroduceBarItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="box">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IntroduceBarItem"
+}
+</script>
+
+<style scoped>
+.box{
+  height: 100%;
+  display: flex;
+}
+.left{
+  position: relative;
+  width: 234px;
+  height: 91%;
+  margin: 8px;
+  background-color: #5e5752;
+}
+.right{
+  flex: 1;
+  height: 91%;
+  margin: 8px;
+}
+</style>
