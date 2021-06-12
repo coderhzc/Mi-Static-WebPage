74b9f9c1086f758cccf259af28db9cbea8806b5e
Mac <mac@MacdeMacBook-Pro.local>
2021-06-12 14:45:41 +0800

    IntroduceBarLocation-Assembly

---
+++ src/components/content/introduceBar/IntroduceBarLocation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="introduce-bar-location">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IntroduceBarLocation"
+}
+</script>
+
+<style scoped>
+.introduce-bar-location{
+  background-color: #fff;
+  width: 100%;
+  margin: auto;
+  padding-left:22px;
+  height: 185px;
+}
+</style>
