63d5d7b0db7668bad369ce0c7e9afb850b1f0e5c
Mac <mac@MacdeMacBook-Pro.local>
2021-06-12 14:30:47 +0800

    HeadView

---
+++ src/views/head/headdviews/HeadView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <main-nav-bar/>
+    <main-top-bar/>
+  </div>
+</template>
+
+<script>
+import MainNavBar from "@/views/head/headdviews/childrenHead/HeadNavBar";
+import MainTopBar from "@/views/head/headdviews/childrenHead/HeadTopBar";
+export default {
+  name: "MainPage",
+  components:{
+    MainNavBar,
+    MainTopBar
+  }
+}
+</script>
+
+<style scoped>
+</style>
