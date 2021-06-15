e748fab022690570d749f3edaa3404bb811cd8e2
Mac <mac@MacdeMacBook-Pro.local>
2021-06-15 13:13:41 +0800

    OneSHowSHopDetailLocation-Assembly

---
+++ src/components/content/OneShowShopDetail/OneShowShopDetailLocation.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="location">
+    <div class="top">
+      <slot name="top"></slot>
+    </div>
+    <div class="bottom">
+      <slot name="bottom"></slot>
+    </div>
+    <div class="advertisement">
+      <slot name="advertisement"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowShopDetailLocation"
+}
+</script>
+
+<style scoped>
+.location{
+  position: relative;
+  width: 100%;
+  height: 820px;
+}
+.top{
+  position: relative;
+  width: 100%;
+  height: 40px;
+}
+.bottom{
+  width: 100%;
+  height: 77%;
+}
+.advertisement{
+  width: 100%;
+  height: 14.5%;
+}
+</style>
