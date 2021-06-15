031a5bf91f830ffbc67b61fe2cc1242640f59a8b
Mac <mac@MacdeMacBook-Pro.local>
2021-06-15 18:20:04 +0800

    ThreeShowShopDetailLocation

---
+++ src/components/content/ThreeShowShopDetail/ThreeShowShopDetailLocation.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="show-shop-detail-location-box">
+    <div class="top">
+      <slot name="top"></slot>
+    </div>
+    <div class="bottom">
+      <slot name="bottom"></slot>
+    </div>
+    <div class="advertisement">
+      <slot name="advertisement"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ThreeShowShopDetailLocation"
+}
+</script>
+
+<style scoped>
+.show-shop-detail-location-box{
+  width: 97%;
+  height: 100%;
+  margin: auto;
+}
+.top{
+  position: relative;
+  width: 100%;
+  height: 80px;
+}
+.bottom{
+  position: relative;
+  width: 100%;
+  height: 630px;
+}
+.advertisement{
+  position: relative;
+  width: 100%;
+  height: 160px;
+}
+</style>
