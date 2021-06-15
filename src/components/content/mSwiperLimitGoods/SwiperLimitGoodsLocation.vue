52c1c2d443537bcc99f90ebab479dc3853e3b535
Mac <mac@MacdeMacBook-Pro.local>
2021-06-15 13:08:08 +0800

    SwiperLimitGoodsLocation-Assembly

---
+++ src/components/content/mSwiperLimitGoods/SwiperLimitGoodsLocation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="limit-goods-location">
+    <slot name="top"></slot>
+    <slot name="bottom"></slot>
+    <slot name="advertisement"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SwiperLimitGoodsLocation"
+}
+</script>
+
+<style scoped>
+.limit-goods-location{
+  position: relative;
+  width: 100%;
+  height: 570px;
+  background-color: #f5f5f5;
+}
+</style>
