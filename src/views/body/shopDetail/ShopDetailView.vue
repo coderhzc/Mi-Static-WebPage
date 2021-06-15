9ffc900a77fb2538330f0440af641060d58669cc
Mac <mac@MacdeMacBook-Pro.local>
2021-06-15 18:18:26 +0800

    Call-ShopDetailView

--- src/views/body/shopDetail/ShopDetailView.vue
+++ src/views/body/shopDetail/ShopDetailView.vue
@@ -2,17 +2,20 @@
   <div>
     <one-shop-detail/>
     <two-shop-detail/>
+    <three-shop-detail/>
   </div>
 </template>
 
 <script>
 import oneShopDetail from "@/views/body/shopDetail/childrenShopDetail/OneShopDetail";
 import TwoShopDetail from "@/views/body/shopDetail/childrenShopDetail/TwoShopDetail";
+import ThreeShopDetail from "@/views/body/shopDetail/childrenShopDetail/ThreeShopDetail";
 export default {
   name: "ShopDetailView",
   components:{
     oneShopDetail,
-    TwoShopDetail
+    TwoShopDetail,
+    ThreeShopDetail
   },
 }
 </script>
