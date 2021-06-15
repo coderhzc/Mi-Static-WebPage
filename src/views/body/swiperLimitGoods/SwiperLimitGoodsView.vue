f866d5ccf7130d5fa0c7bdca5dac517fa5aff777
Mac <mac@MacdeMacBook-Pro.local>
2021-06-15 12:47:21 +0800

    Call-SwiperLimitgoodsView

---
+++ src/views/body/swiperLimitGoods/SwiperLimitGoodsView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <swiper-limit-goods/>
+  </div>
+</template>
+
+<script>
+import SwiperLimitGoods from "@/views/body/swiperLimitGoods/childrenSwiperLimitGoods/SwiperLimitGoods";
+export default {
+  name: "SwiperLimitGoodsView",
+  components:{
+    SwiperLimitGoods,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
