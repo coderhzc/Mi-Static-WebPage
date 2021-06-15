0267bfe95eeef9fe6f873449d5df312532e2f15a
Mac <mac@MacdeMacBook-Pro.local>
2021-06-15 18:16:11 +0800

    Call-FooterView

---
+++ src/views/footer/footerView/FooterView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <footer-bar/>
+  </div>
+</template>
+
+<script>
+import FooterBar from "@/views/footer/footerView/childrenFooterView/FooterBar";
+export default {
+  name: "FooterView",
+  components:{
+    FooterBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
