6e16b80472d933d8dd63d4fb49a363b16b629a6c
Mac <mac@MacdeMacBook-Pro.local>
2021-06-15 12:44:07 +0800

    SwiperControl-Assembly

---
+++ src/views/head/swiperviews/childrenSwiper/SwiperControl.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="control-location">
+    <div class="left-control-location">
+      <div class="left-img-box" :style="isLeftArrowStyle">
+        <img v-if="isLeftArrow" src="~assets/img/MiswiperArrow/leftArrow.svg" alt="">
+        <img v-else src="~assets/img/MiswiperArrow/leftArrow_active.svg" alt="">
+      </div>
+    </div>
+    <div class="right-control-location">
+      <img v-if="isRightArrow" src="~assets/img/MiswiperArrow/rightArrow.svg" alt="">
+      <img v-else src="~assets/img/MiswiperArrow/rightArrow_active.svg" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SwiperControl",
+  data(){
+    return{
+      isLeftArrow:true,
+      isRightArrow:true,
+      isLeftArrowStyle:'',
+      isRightArrowStyle:''
+    }
+  },
+  methods:{
+    LeftArrowEnter(){
+      this.isLeftArrowStyle = "background-color:red"
+      this.isLeftArrow = false
+      console.log(111);
+    },
+  }
+}
+</script>
+
+<style scoped>
+.control-location{
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 81%;
+  height: 15%;
+  margin-top: 175px;
+}
+.left-control-location{
+  width: 4%;
+  height: 100%;
+}
+.left-img-box{
+  width: 100%;
+  height: 100%;
+  padding-top: 15px;
+}
+.left-img-box img{
+  width: 35px;
+}
+.right-control-location{
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 4%;
+  height: 100%;
+  padding-top: 15px;
+  padding-left: 4px;
+
+}
+.right-control-location img{
+  width: 35px;
+}
+</style>
