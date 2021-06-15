35365877bf1eb57605bdb700ac39f63086649e18
Mac <mac@MacdeMacBook-Pro.local>
2021-06-15 12:48:51 +0800

    Call-ShopdetailView

---
+++ src/views/body/shopDetail/ShopDetailView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <one-shop-detail/>
+    <two-shop-detail/>
+  </div>
+</template>
+
+<script>
+import oneShopDetail from "@/views/body/shopDetail/childrenShopDetail/OneShopDetail";
+import TwoShopDetail from "@/views/body/shopDetail/childrenShopDetail/TwoShopDetail";
+export default {
+  name: "ShopDetailView",
+  components:{
+    oneShopDetail,
+    TwoShopDetail
+  },
+}
+</script>
+
+<style scoped>
+</style>
